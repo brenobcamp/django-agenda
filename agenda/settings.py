19eb613daa4999be622a67e5127070ff1fc065d3
Breno Batista Campos <108765626+brenobcamp@users.noreply.github.com>
2022-11-06 16:26:52 -0300

    Update settings.py

--- agenda/settings.py
+++ agenda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p_bprpjpetp3$b+r7m%g!@6+5#yh__iw&q9(c17i^(f)&h+szo'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
